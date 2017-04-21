32639f39a4cc3b6440df0f3a531e76f82c517f1f
lakesare <lakesare@gmail.com>
2017-04-21 09:50:14 +0500

    fetch as google test

--- frontend/pages/test/index.js
+++ frontend/pages/test/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
 // import { Draft } from './components/Draft';
-import { Draft } from './components/DraftImageFromClipboard';
+
+import { commonFetch } from '~/api/commonFetch';
 
 class Page_test extends React.Component {
-  render = () =>
-    <div className="container">
-      <Draft/>
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      speGetPage: { status: 'initial' }
+    };
+  }
+
+  componentDidMount() {
+    commonFetch(
+      spe => this.setState({ speGetPage: spe }),
+      'GET', `/api/pages/courses/5`
+    );
+  }
+
+  render = () => {
+    const spe = this.state.speGetPage;
+    if (spe.status === 'request') return <div>request</div>;
+    if (spe.status === 'failure') return <div>failure</div>;
+    if (spe.status === 'initial') return <div>initial</div>;
+
+    const { problems, course } = spe.payload;
+    return <div className="container">
+      <h1>{course.title}</h1>
+      {
+        problems.map((p) =>
+          <div key={p.id}>{p.type}</div>
+        )
+      }
+    </div>;
+  }
 }
 
 export { Page_test };
