ec3d08a7bbb113b7172b7338ee609572698a3bf7
lakesare <lakesare@gmail.com>
2017-04-21 10:57:51 +0500

    fetch as google test

--- frontend/pages/test/index.js
+++ frontend/pages/test/index.js
@@ -65,34 +65,24 @@ class Page_test extends React.Component {
     };
   }
 
-  componentDidMount() {
-    commonFetch(
-      spe => this.setState({ speGetPage: spe }),
-      'GET', `/api/pages/courses/5`
-    );
-  }
-
-  render = () => {
-
-    const spe = this.state.speGetPage;
-    if (spe.status === 'request') return <div>request</div>;
-    if (spe.status === 'failure') return <div>{spe.error}</div>;
-    if (spe.status === 'initial') return <div>initial</div>;
-
-    const { problems, course } = spe.payload;
-    return <div className="container">
-      <h1>{course.title}</h1>
-      {
-        <Problem
-          mode="viewing"
-          problemContent={problems[0].content}
-          problemType={problems[0].type}
-        />
-      }
-    </div>;
+  render = () =>
+    <div>
+      {botCheck() ? 'true' : 'false'}<br/>
+      {navigator.userAgent} 
+    </div>
+  
+}
 
 
-  }
+function botCheck(){
+  var botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+    var re = new RegExp(botPattern, 'i');
+    var userAgent = navigator.userAgent;
+    if (re.test(userAgent)) {
+      return true;
+    }else{
+      return false;
+    }
 }
 
 export { Page_test };
