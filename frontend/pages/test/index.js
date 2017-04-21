c52527afd6f77ffd5522eee887f1f4eadb1a1242
lakesare <lakesare@gmail.com>
2017-04-21 10:43:44 +0500

    fetch as google test

--- frontend/pages/test/index.js
+++ frontend/pages/test/index.js
@@ -4,6 +4,58 @@ import React from 'react';
 import { Problem } from '~/components/Problem';
 
 import { commonFetch } from '~/api/commonFetch';
+window.onerror = function (message, url, lineNo, colNo, error) {
+
+   console.log(arguments);
+
+   let container = document.createElement('div');
+
+   container.style.color = 'red';
+   container.style.position = 'fixed';
+   container.style.background = '#eee';
+   container.style.padding = '2em';
+   container.style.top = '1em';
+   container.style.left = '1em';
+
+   let msg = document.createElement('pre');
+   msg.innerText = [
+      'Message: ' + message,
+      'URL: ' + url,
+      'Line: ' + lineNo,
+      'Column: ' + colNo,
+      'Stack: ' + (error && error.stack)
+   ].join('\n');
+
+   container.appendChild(msg);
+
+   document.body.appendChild(container);
+};
+
+window.addEventListener("unhandledrejection", function(err, promise) { 
+    // handle error here, for example log   
+
+    console.log(err)
+
+    let container = document.createElement('div');
+
+    container.style.color = 'blue';
+    container.style.position = 'fixed';
+    container.style.background = '#eee';
+    container.style.padding = '2em';
+    container.style.top = '1em';
+    container.style.left = '1em';
+
+    let msg = document.createElement('pre');
+    msg.innerText = [
+      err.reason.message,
+      JSON.stringify(err.reason.stack)
+    ].join('\n');
+
+    container.appendChild(msg);
+
+    document.body.appendChild(container);
+});
+
 
 class Page_test extends React.Component {
   constructor(props) {
@@ -21,6 +73,7 @@ class Page_test extends React.Component {
   }
 
   render = () => {
+
     const spe = this.state.speGetPage;
     if (spe.status === 'request') return <div>request</div>;
     if (spe.status === 'failure') return <div>{spe.error}</div>;
@@ -30,16 +83,15 @@ class Page_test extends React.Component {
     return <div className="container">
       <h1>{course.title}</h1>
       {
-        problems.map((problem) =>
-          <Problem
-            key={problem.id}
-            mode="viewing"
-            problemContent={problem.content}
-            problemType={problem.type}
-          />
-        )
+        <Problem
+          mode="viewing"
+          problemContent={problems[0]}
+          problemType={problems[0].type}
+        />
       }
     </div>;
+
+
   }
 }
 
