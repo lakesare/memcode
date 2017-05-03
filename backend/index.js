31d52557d684f82c34cb40c5d075e12a13186619
lakesare <lakesare@gmail.com>
2017-05-03 09:19:30 +0500

    meta viewport for mobiles

--- backend/index.js
+++ backend/index.js
@@ -51,6 +51,8 @@ app.get('*', (request, response) =>
       <link href="/index.css" rel="stylesheet">
       <!-- to verify google webmasters -->
       <meta name="google-site-verification" content="Cv256pnTnFWM0T6qi3SXK1u1K-B6W7IJQ9JoOQ_1I_E"/>
+      <!-- to make site look bigger on mobiles -->
+      <meta id="meta" name="viewport" content="width=device-width, initial-scale=1.0"/>
 
       <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
       <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
