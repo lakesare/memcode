6171921f12b672b76955cbb9a7ebb8b144880070
lakesare <lakesare@gmail.com>
2018-05-12 02:02:40 +0200

    Footer - made it prettier, added a link to FAQ

--- frontend/components/Footer/index.js
+++ frontend/components/Footer/index.js
@@ -1,10 +1,18 @@
+import { Link } from 'react-router';
 import css from './index.css';
 
 const Footer = () =>
   <footer className={css.footer}>
     <div className="container">
-      {window.env.contactEmail}
-      <i className="fa fa-envelope"/>
+      <div className="faq">
+        <i className="fa fa-anchor"/>
+        <Link to="/articles/welcome">FAQ</Link>
+      </div>
+
+      <div className="email">
+        {window.env.contactEmail}
+        <i className="fa fa-heart"/>
+      </div>
     </div>
   </footer>;
 
