ef98922aefbe8d1cba90370fd9f1e458baeb2774
lakesare <lakesare@gmail.com>
2018-09-06 02:55:35 +0200

    /courses/:id/edit - added tooltip tutorials, added validation for empty flashcard creation

--- frontend/pages/courses_id_edit/components/NewProblem.js
+++ frontend/pages/courses_id_edit/components/NewProblem.js
@@ -1,5 +1,7 @@
 import * as ProblemApi from '~/api/Problem';
+import * as speCreator from '~/services/spe';
 import { Problem } from '~/components/Problem';
+import { Loading } from '~/components/Loading';
 
 const createEmptyEditorState = (type) => {
   switch (type) {
@@ -40,23 +42,47 @@ class NewProblem extends React.Component {
     }
   }
 
-  apiSave = () => {
+  uiValidate = () => {
     const type = this.state.currentProblemType;
-
-    ProblemApi.create(
-      (spe) => this.setState({ speCreateProblem: spe }),
-      {
-        content: this.state.problemContent,
-        type,
-        courseId: this.props.courseId
+    const problemContent = this.state.problemContent;
+    let error = '';
+    if (type === 'separateAnswer') {
+      if (!problemContent.content) {
+        error = "Please add the question (you'll be asked it when you review the flashcard).";
+      } else if (!problemContent.answer) {
+        error = "Please add the answer to the question.";
       }
-    )
-      .then((createdProblem) => {
-        this.props.addNewProblem(createdProblem);
-        this.setState({
-          problemContent: createEmptyEditorState(this.state.currentProblemType)
+    } else if (type === 'inlinedAnswers') {
+      if (!problemContent.content) {
+        error = "Please add some sentence with a word that you'll need to fill in on review (select words you'd like to fill in, and press Mark As Answer).";
+      }
+    }
+    if (error) {
+      this.setState({ speCreateProblem: speCreator.failure(error) });
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  apiSave = () => {
+    if (this.uiValidate()) {
+      const type = this.state.currentProblemType;
+      ProblemApi.create(
+        (spe) => this.setState({ speCreateProblem: spe }),
+        {
+          content: this.state.problemContent,
+          type,
+          courseId: this.props.courseId
+        }
+      )
+        .then((createdProblem) => {
+          this.props.addNewProblem(createdProblem);
+          this.setState({
+            problemContent: createEmptyEditorState(this.state.currentProblemType)
+          });
         });
-      });
+    }
   }
 
   updateProblemContent = (problemContent) =>
@@ -111,6 +137,8 @@ class NewProblem extends React.Component {
         <span>CTRL+S to save a new flashcard</span>
         <button className="button -pink" onClick={this.apiSave}>SAVE</button>
       </section>
+
+      <Loading enabledStatuses={['failure']} spe={this.state.speCreateProblem}/>
 
       <section className="choose-type">
         {this.renderTypeButton('separateAnswer', 'question answer')}
--- frontend/pages/courses_id_edit/index.js
+++ frontend/pages/courses_id_edit/index.js
@@ -1,6 +1,6 @@
 import { update } from 'lodash';
 
-import { Link } from 'react-router';
+import Joyride from 'react-joyride';
 import { StickyContainer, Sticky } from 'react-sticky';
 import { Header }  from '~/components/Header';
 import { Footer } from '~/components/Footer';
@@ -40,11 +40,53 @@ class Page_courses_id_edit extends React.Component {
 
   state = {
     speGetPage: {},
-    idsOfCheckedProblems: []
+    idsOfCheckedProblems: [],
+    joyrideHowToCreateProblemRun: false,
+    joyrideHowToCreateProblemSteps: [
+      {
+        target: 'section.choose-type',
+        content: 'You can create 2 types of flashcards',
+        placement: 'bottom'
+      },
+      {
+        target: '.new-problem .first-column',
+        content: "Enter the question, you will be asked this question when you're learning the flashcards (What's 2x2?)",
+        placement: 'bottom'
+      },
+      {
+        target: '.new-problem .second-column',
+        content: "Enter the answer (4)",
+        placement: 'bottom'
+      },
+      {
+        target: '.new-problem .how-to-create button',
+        content: "Save the new flashcard!",
+        placement: 'bottom'
+      }
+    ],
+    joyrideHowToReviewProblemRun: false,
+    joyrideHowToReviewProblemSteps: [
+      {
+        target: 'section.title-and-buttons a.learn',
+        content: "Learn the created flashcard!",
+        placement: 'bottom'
+      },
+      {
+        target: 'section.title-and-buttons a.review',
+        content: 'After you learn the flashcard - we will offer you to Review them from time to time!',
+        placement: 'bottom'
+      }
+    ]
   }
 
-  componentDidMount = () =>
+  componentDidMount = () => {
     this.apiGetPage()
+      .then(() => {
+        setTimeout(() => {
+          this.setState({ joyrideHowToCreateProblemRun: true });
+        }, 3000);
+      });
+  }
 
   apiGetPage = () =>
     commonFetch(
@@ -60,6 +102,9 @@ class Page_courses_id_edit extends React.Component {
       )
     });
     this.props.IdsOfProblemsToLearnAndReviewPerCourseActions.createProblem(this.props.params.id, createdProblem.id);
+    setTimeout(() => {
+      this.setState({ joyrideHowToReviewProblemRun: true });
+    }, 3000);
   }
 
   updateOldProblem = (updatedProblem) => {
@@ -104,36 +149,66 @@ class Page_courses_id_edit extends React.Component {
     </div>
   )
 
-  renderProblems = (problems) =>
-    <section className="problems">
-      {problems.map((problem, index) =>
-        <OldProblem
-          key={problem.id}
-          problem={problem}
-          index={index}
-          updateOldProblem={this.updateOldProblem}
-          removeOldProblem={this.removeOldProblem}
-          idsOfCheckedProblems={this.state.idsOfCheckedProblems}
-          updateIdsOfCheckedProblems={(ids) => this.setState({ idsOfCheckedProblems: ids })}
-        />
-      )}
-      <NewProblem courseId={this.props.params.id} addNewProblem={this.addNewProblem}/>
-    </section>
+  renderProblems = () =>
+    <Loading spe={this.state.speGetPage}>{({ problems }) =>
+      <section className="problems">
+        {problems.map((problem, index) =>
+          <OldProblem
+            key={problem.id}
+            problem={problem}
+            index={index}
+            updateOldProblem={this.updateOldProblem}
+            removeOldProblem={this.removeOldProblem}
+            idsOfCheckedProblems={this.state.idsOfCheckedProblems}
+            updateIdsOfCheckedProblems={(ids) => this.setState({ idsOfCheckedProblems: ids })}
+          />
+        )}
+      </section>
+    }</Loading>
 
   render = () =>
     <main className={css.main}>
+      <Joyride
+        steps={this.state.joyrideHowToCreateProblemSteps}
+        run={this.state.joyrideHowToCreateProblemRun}
+        // callback={this.callback}
+        hideBackButton
+        // disableOverlay
+        styles={{
+          options: {
+            arrowColor: '#ffffff',
+            backgroundColor: '#ffffff',
+            primaryColor: '#ec60ac',
+            textColor: '#004a14',
+            width: 200,
+          }
+        }}
+      />
+      <Joyride
+        steps={this.state.joyrideHowToReviewProblemSteps}
+        run={this.state.joyrideHowToReviewProblemRun}
+        hideBackButton
+        styles={{
+          options: {
+            arrowColor: '#ffffff',
+            backgroundColor: '#ffffff',
+            primaryColor: '#ec60ac',
+            textColor: '#004a14',
+            width: 200,
+          }
+        }}
+      />
       <Header dontLinkToLearnOrReview={this.props.params.id}/>
 
       <CourseActions courseId={this.props.params.id} ifEditCourseModalTogglerIsDisplayed ifCourseDescriptionIsDisplayed ifBreadcrumbsAreDisplayed ifConfused/>
-      <div className="container">
-        <Loading spe={this.state.speGetPage}>{({ problems }) =>
-          <StickyContainer>
-            {this.renderActionsForCheckedProblems()}
-            {this.renderProblems(problems)}
-            <Instructions/>
-          </StickyContainer>
-        }</Loading>
-      </div>
+      <StickyContainer>
+        <div className="container">
+          {this.renderActionsForCheckedProblems()}
+          {this.renderProblems()}
+          <NewProblem courseId={this.props.params.id} addNewProblem={this.addNewProblem}/>
+          <Instructions/>
+        </div>
+      </StickyContainer>
 
       <Footer/>
     </main>
