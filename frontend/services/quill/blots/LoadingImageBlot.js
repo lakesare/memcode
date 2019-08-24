01b09a4c3608425a3c47278271ea3cfafac2d2ed
lakesare <lakesare@gmail.com>
2019-08-25 02:07:35 +0500

    /courses/:id/edit - added image insertion that doesn't fuck up further text!

--- frontend/services/quill/blots/LoadingImageBlot.js
+++ frontend/services/quill/blots/LoadingImageBlot.js
@@ -2,9 +2,11 @@ import { Quill } from 'react-quill';
 
 const BlockEmbed = Quill.import('blots/block/embed');
 class LoadingImageBlot extends BlockEmbed {
-  static create({ src, className = "" }) {
+  static create({ src, className, id }) {
+    // node is a <section>
     const node = super.create();
     node.setAttribute('class', className);
+    node.setAttribute('data-id', id);
 
     const img = document.createElement('img');
     img.setAttribute('src', src);
--- frontend/services/quill/handlers/services/placeholdAndCreateImage.js
+++ frontend/services/quill/handlers/services/placeholdAndCreateImage.js
@@ -1,4 +1,6 @@
 import Delta from 'quill-delta';
+import Parchment from 'parchment';
+import _ from 'lodash';
 
 import fromFileToDataUrl from '~/services/fromFileToDataUrl';
 import preloadImage from '~/services/preloadImage';
@@ -12,19 +14,27 @@ const placeholdAndCreateImage = (quill, file) => {
     // when we are not focused on the editor (e.g. when we just drop something)
     quill.getLength();
 
+  const randomId = _.uniqueId();
+
   fromFileToDataUrl(file, (dataUrl) => {
     quill.updateContents(
       new Delta()
         .retain(selectionAt)
-        .insert({ loadingImage: { src: dataUrl, className: 'placeholder-for-loading-image' } })
+        .insert({ loadingImage: { src: dataUrl, className: 'placeholder-for-loading-image', id: randomId } })
     );
     FileApi.upload(false, file)
       .then((response) => {
         preloadImage(response.url, () => {
+          console.log(quill);
+          const placeholderEl = quill.container.querySelector(`section.placeholder-for-loading-image[data-id="${randomId}"]`);
+
+          const blot = Parchment.find(placeholderEl);
+          const index = blot.offset(quill.scroll);
+
           quill.updateContents(
             new Delta()
-              .retain(selectionAt)
-              .delete(2) // delete the placeholder
+              .retain(index)
+              .delete(2) // delete the placeholder (I'm not sure why .delete(1) doesn't work)
               .insert({ image: response.url })
           );
         });
