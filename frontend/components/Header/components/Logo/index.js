3de9f6e493919812a444185beac8c76cff139f25
lakesare <lakesare@gmail.com>
2019-12-22 01:03:33 +0000

    Traditional Christmas hat, no to biohazard signs!

--- frontend/components/Header/components/Logo/index.js
+++ frontend/components/Header/components/Logo/index.js
@@ -1,24 +1,14 @@
 import { Link } from 'react-router-dom';
 
-import missle from './missle.svg';
+// import missle from './missle.svg';
 
 // <img src={memcodeLogo}/>
 class Logo extends React.Component {
   render = () =>
-    <Link className="logo" to="/" style={{ position: 'relative' }}>
+    <Link className="logo" to="/">
       MEMCODE
 
-      <img
-        style={{
-          width: 44,
-          position: 'absolute',
-          opacity: 0.85,
-          transform: 'rotate(10deg)',
-          right: -42,
-          top: 0
-        }}
-        src={missle}
-      />
+      <img className="christmas-icon" src="https://images.vexels.com/media/users/3/133845/isolated/preview/d582812546109335e90d25dd6b5f26e6-red-santa-claus-hat-flat-icon-16-by-vexels.png"/>
     </Link>
 }
 
