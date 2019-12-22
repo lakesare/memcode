cd939fc524f5eada9b13a802c8f772f22a484ea6
lakesare <lakesare@gmail.com>
2019-12-22 00:46:36 +0000

    preliminary Christmas icon!

--- frontend/components/Header/components/Logo/index.js
+++ frontend/components/Header/components/Logo/index.js
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom';
 
-// import memcodeLogo from './memcodeLogo.svg';
+import missle from './missle.svg';
 
 // <img src={memcodeLogo}/>
 class Logo extends React.Component {
   render = () =>
-    <Link className="logo" to="/">
+    <Link className="logo" to="/" style={{ position: 'relative' }}>
       MEMCODE
+
+      <img
+        style={{
+          width: 62,
+          position: 'absolute',
+          opacity: 0.85,
+          transform: 'rotate(20deg)',
+          left: '-60px',
+          top: '-4px'
+        }}
+        src={missle}
+      />
     </Link>
 }
 
