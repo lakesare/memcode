f400fb9a535e5bbca12fb0f6e6e8436fe665342c
lakesare <lakesare@gmail.com>
2020-01-26 22:26:18 +0500

    <Editor/> - added instructions for shortcuts :-o

--- frontend/components/Editor/index.js
+++ frontend/components/Editor/index.js
@@ -9,6 +9,8 @@ import subScriptHandler        from '~/services/quill/handlers/subScriptHandler'
 
 import ReactQuill from 'react-quill';
 
+import { tippy } from '@tippy.js/react';
+
 // ___Why is it not defined inside the render() function?
 //    We must define it here, otherwise we'll be getting infinite loops!
 const bindings = {
@@ -63,6 +65,15 @@ const bindings = {
   }
 };
 
+const tip = (button, explanation, shortcut, instruction) => {
+  const explanationDiv = `<div class="explanation">${explanation}</div>`;
+  const shortcutDiv = shortcut ? `<div class="shortcut">${shortcut}</div>` : '';
+  const instructionDiv = instruction ? `<div class="instruction">${instruction}</div>` : '';
+  tippy(button, {
+    content: `<div class="quill-toolbar-tooltip">${explanationDiv} ${shortcutDiv} ${instructionDiv}</div>`
+  });
+};
+
 class Editor extends React.Component {
   static propTypes = {
     editorState: PropTypes.string.isRequired,
@@ -107,6 +118,36 @@ class Editor extends React.Component {
     const buttons = quillElement.querySelectorAll('.ql-toolbar button');
 
     buttons.forEach((button) => button.setAttribute('tabindex', '-1'));
+
+    buttons.forEach((button) => {
+      switch (button.className) {
+        case 'ql-bold': { tip(button, 'Bold', '⌘ B'); break; }
+        case 'ql-code':       { tip(button, 'Inline code'); break; }
+        case 'ql-blockquote': { tip(button, 'Quote', '⌘ Shift K'); break; }
+        case 'ql-code-block': { tip(button, 'Block of code', '⌘ Shift C'); break; }
+        case 'ql-link':       { tip(button, 'Link', null, 'Select some text and click here'); break; }
+        case 'ql-image':      { tip(button, 'Image', null, 'You can also paste/drop your image'); break; }
+        case 'ql-clean':      { tip(button, 'Clear formatting'); break; }
+        case 'ql-formula':    { tip(button, 'Insert KaTeX formula'); break; }
+        case 'ql-answer':     { tip(button, 'Answer', '⌘ Shift ENTER', 'Select some text, and mark it as an answer'); break; }
+        case 'ql-list': {
+          if (button.value === 'ordered') {
+            tip(button, 'Ordered List', null, 'Type \'1.\'');
+          } else if (button.value === 'bullet') {
+            tip(button, 'Unordered List', null, 'Type \'-\'');
+          }
+          break;
+        }
+        case 'ql-script': {
+          if (button.value === 'sub') {
+            tip(button, 'Subscript', '⌘ Shift ,');
+          } else if (button.value === 'super') {
+            tip(button, 'Superscript', '⌘ Shift .');
+          }
+          break;
+        }
+      }
+    });
   }
 
   onBlur = () => {
