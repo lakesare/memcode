9c6b7e5f1f4ce563b8527ddc952603e4f155055a
lakesare <lakesare@gmail.com>
2020-02-13 02:19:57 +0500

    <Header/> - removed christmas icon

--- frontend/appComponents/Header/components/Logo/index.js
+++ frontend/appComponents/Header/components/Logo/index.js
@@ -7,8 +7,6 @@ class Logo extends React.Component {
   render = () =>
     <Link className="logo" to="/">
       MEMCODE
-
-      <img className="christmas-icon" src="https://images.vexels.com/media/users/3/133845/isolated/preview/d582812546109335e90d25dd6b5f26e6-red-santa-claus-hat-flat-icon-16-by-vexels.png"/>
     </Link>
 }
 
