221178d694745f7ccd2f3e8eaceccfc0b0abdf8a
lakesare <lakesare@gmail.com>
2020-02-16 18:40:44 +0500

    <CourseCategoryModel/> - stable ordering

--- frontend/models/CourseCategoryModel.js
+++ frontend/models/CourseCategoryModel.js
@@ -1,29 +1,21 @@
-const sortByAmountOfCoursesFn = (a, b) => {
-  if (a.amountOfCourses > b.amountOfCourses) {
-    return -1;
-  } else if (a.amountOfCourses < b.amountOfCourses) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
-
-const sortByAlphabetFn = (a, b) => {
-  if (a.name < b.name) {
-    return -1;
-  } else if (a.name > b.name) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
-
 const deriveAndSortCategoriesPerGroup = (categories, group) =>
   categories
     .filter((category) => category.courseCategoryGroupId === group.id)
-    .sort((a, b) => sortByAmountOfCoursesFn(a, b) || sortByAlphabetFn(a, b));
+    .sort((a, b) => {
+      const order = [
+        'Programming Languages', 'Computer Science', 'Mathematics', 'Biology', 'Physics',
+        'Psychology', 'Politics', 'Economics', 'Law', 'History', 'Music', 'Literature', 'Philosophy', 'Psychology',
+        'English', 'German', 'Spanish',
+        'Other'
+      ];
+
+      if (order.indexOf(a.name) < order.indexOf(b.name)) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
 
 export default {
-  sortByAmountOfCoursesFn, sortByAlphabetFn,
   deriveAndSortCategoriesPerGroup
 };
