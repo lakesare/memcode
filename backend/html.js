86fce9e30ed539e6b22a94d7c016b3f6c1c40e0c
lakesare <lakesare@gmail.com>
2020-03-28 17:51:01 +0500

    html.js - removed ?cache-bust

--- backend/html.js
+++ backend/html.js
@@ -22,7 +22,7 @@ const html = `
     <title>Memcode</title>
 
     <!-- css -->
-    <link href="/index.css?cacheBust-2" rel="stylesheet">
+    <link href="/index.css" rel="stylesheet">
 
     <!-- katex support -->
     <link rel="stylesheet" href="/nonNpmLibraries/katex.min.css"/>
@@ -58,7 +58,7 @@ const html = `
     </script>
 
     <!-- main js file -->
-    <script type="text/javascript" src="/index.js?cacheBust-2" defer></script>
+    <script type="text/javascript" src="/index.js" defer></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
 
