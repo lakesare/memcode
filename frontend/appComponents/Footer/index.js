3e435766cd18bd6b7781af7949cd2ed941063975
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-03-28 16:58:29 +0000

    Ref #53 - refactored code

--- frontend/appComponents/Footer/index.js
+++ frontend/appComponents/Footer/index.js
@@ -3,16 +3,19 @@ import css from './index.css';
 
 import ThemeToggleButton from '~/appComponents/ThemeToggleButton';
 
+var target="_blank"; // to launch link in new tab
+var rel="noopener noreferrer"; // recommended when link refers to external site
+
 class Footer extends React.Component {
   render = () =>
     <footer className={css.footer}>
       <div className="container">
         <section className="blog-links">
-          <Link to="/articles/comparison" target="_blank" rel="noopener noreferrer" >Blog</Link>
+          <Link to="/articles/comparison" target={target}>Blog</Link>
           <span className="pipe">|</span>
-          <Link to="/contact" target="_blank" rel="noopener noreferrer">Contact</Link>
+          <Link to="/contact" target={target}>Contact</Link>
           <span className="pipe">|</span>
-          <a href="https://github.com/lakesare/memcode" target="_blank" rel="noopener noreferrer">Github</a>
+          <a href="https://github.com/lakesare/memcode" target={target} rel={rel}>Github</a>
         </section>
 
         <section className="email">
