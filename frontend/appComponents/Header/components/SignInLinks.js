c9723511e8b5653d731a8396b8a6447b9bea8299
CoderToCode <sagarpatel3012000@gmail.com>
2020-04-10 01:45:45 +0530

    Fixed issue #27 - After user signs in - they should be redirected back

--- frontend/appComponents/Header/components/SignInLinks.js
+++ frontend/appComponents/Header/components/SignInLinks.js
@@ -1,13 +1,22 @@
+import { withRouter } from 'react-router-dom';
+
+@withRouter
+
 class SignInLinks extends React.Component {
+  
+  redirectBackTo = () => {
+    sessionStorage.setItem("lastpage",this.props.location.pathname);
+  }
+
   render = () =>
     <section className="sign-in-links">
       <label className="soliciting">Sign in:</label>
       <div className="links">
-        <a className="gmail" href={window.env.googleSignInLink}>
+        <a className="gmail" onClick={this.redirectBackTo} href={window.env.googleSignInLink}>
           <span className="name">Gmail</span>
           <i className="fa fa-google"/>
         </a>
-        <a className="github" href={window.env.githubSignInLink}>
+        <a className="github" onClick={this.redirectBackTo} href={window.env.githubSignInLink}>
           <span className="name">Github</span>
           <i className="fa fa-github"/>
         </a>
--- frontend/appComponents/SignInButtons/index.js
+++ frontend/appComponents/SignInButtons/index.js
@@ -1,6 +1,14 @@
 import css from './index.css';
+import { withRouter } from 'react-router-dom';
+
+@withRouter
 
 class Page_pleaseSignIn extends React.Component {
+    
+  redirectBackTo = () => {
+    sessionStorage.setItem("lastpage","/courses/new");
+  }
+
   static propTypes = {
     text: PropTypes.string
   }
@@ -11,10 +19,10 @@ class Page_pleaseSignIn extends React.Component {
 
   render = () =>
     <div className={`sign-in-buttons ${css.local}`}>
-      <a className="sign-in button -move-up-on-hover -gmail" href={window.env.googleSignInLink}>
+      <a className="sign-in button -move-up-on-hover -gmail" onClick={this.redirectBackTo} href={window.env.googleSignInLink}>
         {this.props.text} gmail <i className="fa fa-google"/>
       </a>
-      <a className="sign-in button -move-up-on-hover -github" href={window.env.githubSignInLink}>
+      <a className="sign-in button -move-up-on-hover -github" onClick={this.redirectBackTo} href={window.env.githubSignInLink}>
         {this.props.text} github <i className="fa fa-github"/>
       </a>
     </div>
