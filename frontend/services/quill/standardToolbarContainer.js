157824de88d2ec7a8db6055942387ec70fc66b29
lakesare <lakesare@gmail.com>
2020-07-26 16:46:18 +0500

    <Editor/> - enable nested lists!

--- frontend/services/quill/standardToolbarContainer.js
+++ frontend/services/quill/standardToolbarContainer.js
@@ -2,7 +2,6 @@ const standardToolbarContainer = [
   ['bold', { script: 'sub' }, { script: 'super' }, 'code'],
   ['blockquote', 'code-block'],
   [{ list: 'ordered' }, { list: 'bullet' }],
-  [{ 'indent': '-1'}, { 'indent': '+1' }],
   ['link', 'image'],
   // [{ 'size': ['small', false, 'large', 'huge'] }],
   // [{ 'color': [] }, { 'background': [] }],
