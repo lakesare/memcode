bc861bd0ec921ba402d5420f67496482e9aa6762
Daniel Eder <1525711+daniel-eder@users.noreply.github.com>
2021-04-05 13:16:31 +0000

    Add hotkey and handler for KaTeX formula

---
+++ frontend/services/quill/handlers/formulaHandler.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+function formulaHandler (range, context) {
+    //based on this.quill.theme.tooltip.edit('formula');
+    this.quill.theme.tooltip.edit('formula');
+}
+
+export default formulaHandler;
