2837b1dc2ee24531eef468d7ba9ee60ddec08888
lakesare <lakesare@gmail.com>
2021-08-24 19:06:01 +0500

    /review - nice nonjumpy 'By Heart'

--- frontend/pages/courses_id_review/components/ProblemBeingSolved/components/Subheader/index.js
+++ frontend/pages/courses_id_review/components/ProblemBeingSolved/components/Subheader/index.js
@@ -109,7 +109,7 @@ class Subheader extends React.Component {
       <div className="container">
         <div className="instructions -desktop">
           <em className="review-emphasis">REVIEW</em> - we are repeating due flashcards <em className="white-emphasis">☘</em>.
-          Press <span style={{ color: 'rgb(134, 244, 159)' }}>ENTER</span> to reveal answers.
+          Press <em className="review-emphasis">ENTER</em> to reveal answers.
           Use arrows <span style={{ color: 'rgb(242, 112, 110)' }}>◄</span> <span style={{ color: 'rgb(134, 244, 159)' }}>►</span> to rate yourself.
         </div>
 
