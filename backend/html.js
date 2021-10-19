07bbe6250054fe31ed5506ce460e5e439620940c
lakesare <lakesare@gmail.com>
2021-10-19 13:01:39 +0500

    mobile - fix the background to dark

--- backend/html.js
+++ backend/html.js
@@ -37,10 +37,9 @@ const html = `
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ba2490">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg">
     <link rel="manifest" href="/site.webmanifest">
     <meta name="msapplication-TileColor" content="#9f00a7">
-    <meta name="theme-color" content="#ffffff">
 
     <!-- data-react-helmet="true" for react-helmet to supersede it instead of adding a new description tag -->
     <meta name="description" content="Create your own course to memorize anything you want. Flashcard-based, with formatting and images, with multiple flashcard types." data-react-helmet="true"/>
