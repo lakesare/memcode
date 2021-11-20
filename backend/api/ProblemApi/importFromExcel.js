c07f1c9e213e131be100fa216aac8e6f6fe27d37
lakesare <lakesare@gmail.com>
2021-11-20 11:53:06 +0500

    /api/importFromExcel - fix the ordering reversal

--- backend/api/ProblemApi/importFromExcel.js
+++ backend/api/ProblemApi/importFromExcel.js
@@ -1,11 +1,19 @@
 import db from '~/db/init.js';
+// import knex from '~/db/knex';
 
 const importFromExcel = async (request, response) => {
   const courseId = request.body['courseId'];
   const problems = request.body['problems'];
 
+  // We should always assign the goddamn position.
+  // const currentlyNOfProblems = knex('problem').where({ course_id: courseId })
+  //   // Put position-0 last (because it means they were created after the latest reordering!) (https://stackoverflow.com/a/3130216/3192470)
+  //   .orderByRaw('position=0')
+  //   .orderBy('position')
+  //   .orderBy('createdAt', 'asc');
+
   const arrayOfNulls = await db.tx((transaction) => {
-    const queries = problems.map((problem) =>
+    const queries = problems.map((problem, index) =>
       transaction.none(
         "INSERT INTO problem (type, content, course_id, created_at) VALUES (${type}, ${content}, ${courseId}, now())",
         {
@@ -14,6 +22,7 @@ const importFromExcel = async (request, response) => {
             content: problem.content,
             answer: problem.answer
           },
+          position: index + 1,
           courseId
         }
       )
