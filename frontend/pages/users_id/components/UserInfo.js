999174c89793dbbea977313f104e90f979565af3
lakesare <lakesare@gmail.com>
2022-06-18 18:56:00 +0200

    /users/:id - refactor

---
+++ frontend/pages/users_id/components/UserInfo.js
@@ -0,0 +1,158 @@
+import capitalize from '~/services/capitalize';
+
+import { Pie } from 'react-chartjs-2';
+import Loading from '~/components/Loading';
+
+import { Chart, ArcElement, Tooltip } from "chart.js";
+Chart.register(ArcElement, Tooltip);
+
+class UserInfo extends React.Component {
+  static propTypes = {
+    speGetPage: PropTypes.object.isRequired
+  }
+
+  getDate = (date) => {
+    return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  }
+
+  renderUser = (user) =>
+    <div className="user">
+      <div className="user-details">
+        <h1>Profile</h1>
+        <img src={user.avatarUrl} alt="avatar"/>
+        <div className="right">
+          <div className="username">{capitalize(user.username)}</div>
+
+          {
+            user.email &&
+            <div className="email">{user.email}</div>
+          }
+
+          <div className="created-at">
+            Joined {this.getDate(user.createdAt)}
+          </div>
+        </div>
+      </div>
+    </div>
+
+  renderSkills = (skills) => {
+    let max;
+    if (!skills[0]) {
+      skills = [
+        { categoryName: 'Computer Science', nOfFlashcards: 0 },
+        { categoryName: 'Mathematics', nOfFlashcards: 0 },
+        { categoryName: 'Biology', nOfFlashcards: 0 },
+        { categoryName: 'Politics', nOfFlashcards: 0 },
+        { categoryName: 'Literature', nOfFlashcards: 0 },
+      ];
+      max = 1;
+    } else {
+      max = skills[0].nOfFlashcards;
+    }
+
+    return <div className="skills">
+      <h1>Skills</h1>
+      {skills.map((skill) =>
+        <div className="skill" key={skill.categoryName}>
+          <h2>{skill.categoryName}</h2>
+
+          <section className="progress-bar">
+            <span className="n-of-flashcards">{skill.nOfFlashcards} flashcards</span>
+            <div className="inner" style={{ width: ((skill.nOfFlashcards / max) * 100).toString() + '%' }}/>
+          </section>
+        </div>
+      )}
+    </div>;
+  }
+
+  renderStats = (stats) => {
+    // const max = skills[0].nOfFlashcards;
+    const greenLong = '#09cc54';
+    const greenMiddle = '#6fca92';
+    const greenShort = '#9cc4ab';
+
+    const data = {
+      labels: [
+        '  Long-term memories',
+        '  Middle-term memories',
+        '  Fresh memories'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: stats.easiness.longTerm === 0 && stats.easiness.middleTerm === 0 && stats.easiness.shortTerm === 0 ?
+          [0, 0, 1] :
+          [stats.easiness.longTerm, stats.easiness.middleTerm, stats.easiness.shortTerm],
+        backgroundColor: [greenLong, greenMiddle, greenShort],
+        hoverOffset: 0,
+        borderColor: 'rgba(255, 255, 255, 1)',
+        borderWidth: 1,
+        hoverBackgroundColor: [greenLong, greenMiddle, greenShort]
+      }],
+      // responsive: true,
+      // maintainAspectRatio: false
+    };
+
+    const toPercent = (easinessFor) =>
+      stats.nOfProblemsLearned === 0 ?
+        0 :
+        Math.round((stats.easiness[easinessFor] / stats.nOfProblemsLearned) * 100, 2);
+
+    return <div className="stats">
+      <h1>Progress</h1>
+
+      <div className="wrapper">
+        <div className="left" style={{ width: 200, marginTop: 8 }}>
+          <Pie data={data}/>
+        </div>
+
+        <div className="right" style={{ marginTop: 12 }}>
+          <ul className="textual-stats">
+            <li className="stat">
+              {stats.nOfCoursesCreated} courses created
+            </li>
+
+            <li className="stat">
+              {stats.nOfProblemsLearned} flashcards learned:
+              <ul className="memory-levels">
+                <li>
+                  <div className="square" style={{ background: greenLong }}/>
+                  {toPercent('longTerm')}% in long-term memory
+                </li>
+                <li>
+                  <div className="square" style={{ background: greenMiddle }}/>
+                  {toPercent('middleTerm')}% in middle-term memory
+                </li>
+                <li>
+                  <div className="square" style={{ background: greenShort }}/>
+                  {toPercent('shortTerm')}% freshly learned
+                </li>
+              </ul>
+            </li>
+
+            {/* <li className="stat"> */}
+            {/*   Soon: */}
+            {/*   <ul className="memory-levels"> */}
+            {/*     <li>+ 0 flashcards to review in the next hour</li> */}
+            {/*     <li>+ 20 flashcards to review in the next 24 hours</li> */}
+            {/*     <li>+ 300 flashacards to review in the next week</li> */}
+            {/*   </ul> */}
+            {/* </li> */}
+          </ul>
+        </div>
+      </div>
+
+
+    </div>;
+  }
+
+  render = () =>
+    <Loading spe={this.props.speGetPage}>{({ user, skills, stats }) =>
+      <div className="wrapper">
+        {this.renderUser(user)}
+        {this.renderSkills(skills)}
+        {this.renderStats(stats)}
+      </div>
+    }</Loading>
+}
+
+export default UserInfo;
--- frontend/pages/users_id/index.js
+++ frontend/pages/users_id/index.js
@@ -5,34 +5,23 @@ import api from '~/api';
 import capitalize from '~/services/capitalize';
 import orFalse from '~/services/orFalse';
 
-import Courses from './components/Courses';
 import Loading from '~/components/Loading';
 import Main from '~/appComponents/Main';
+import UserInfo from './components/UserInfo';
+import Courses from './components/Courses';
 
 import css from './index.css';
 
-import { Pie } from 'react-chartjs-2';
-
-import { Chart, ArcElement, Tooltip } from "chart.js";
-Chart.register(ArcElement, Tooltip);
-
-
-import { AuthenticationActions } from '~/reducers/Authentication';
 @withRouter
 @connect(
   (state) => ({
     currentUser: state.global.Authentication.currentUser
   }),
-  (dispatch) => ({
-    signIn: (token) => AuthenticationActions.signIn(dispatch, token)
-  })
 )
 class Page_users_id extends React.Component {
   static propTypes = {
-    history: PropTypes.object.isRequired,
     match: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
-    signIn: PropTypes.func.isRequired,
     currentUser: orFalse(PropTypes.object).isRequired,
   }
 
@@ -50,16 +39,6 @@ class Page_users_id extends React.Component {
     }
   }
 
-  // tryToFindToken = () => {
-  //   const queryParams = new URLSearchParams(window.location.search);
-  //   const token = queryParams.get('token');
-  //   if (token) {
-  //     this.props.signIn(token);
-  //     this.props.history.push(`/users/${this.props.currentUser.id}`);
-  //     
-  //   }
-  // }
-
   apiGetPage = () => {
     api.PageApi.getUserPage(
       (spe) => this.setState({ speGetPage: spe }),
@@ -67,151 +46,13 @@ class Page_users_id extends React.Component {
     );
   }
 
-  getDate = (date) => {
-    return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-  }
-
-  renderUser = (user) =>
-    <div className="user">
-      <div className="user-details">
-        <h1>Profile</h1>
-        <img src={user.avatarUrl} alt="avatar"/>
-        <div className="right">
-          <div className="username">{capitalize(user.username)}</div>
-
-          {
-            user.email &&
-            <div className="email">{user.email}</div>
-          }
-
-          <div className="created-at">
-            Joined {this.getDate(user.createdAt)}
-          </div>
-        </div>
-      </div>
-    </div>
-
-  renderSkills = (skills) => {
-    let max;
-    if (!skills[0]) {
-      skills = [
-        { categoryName: 'Computer Science', nOfFlashcards: 0 },
-        { categoryName: 'Mathematics', nOfFlashcards: 0 },
-        { categoryName: 'Biology', nOfFlashcards: 0 },
-        { categoryName: 'Politics', nOfFlashcards: 0 },
-        { categoryName: 'Literature', nOfFlashcards: 0 },
-      ];
-      max = 1;
-    } else {
-      max = skills[0].nOfFlashcards;
-    }
-
-    return <div className="skills">
-      <h1>Skills</h1>
-      {skills.map((skill) =>
-        <div className="skill" key={skill.categoryName}>
-          <h2>{skill.categoryName}</h2>
-
-          <section className="progress-bar">
-            <span className="n-of-flashcards">{skill.nOfFlashcards} flashcards</span>
-            <div className="inner" style={{ width: ((skill.nOfFlashcards / max) * 100).toString() + '%' }}/>
-          </section>
-        </div>
-      )}
-    </div>;
-  }
-
-  renderStats = (stats) => {
-    // const max = skills[0].nOfFlashcards;
-    const greenLong = '#09cc54';
-    const greenMiddle = '#6fca92';
-    const greenShort = '#9cc4ab';
-
-    const data = {
-      labels: [
-        '  Long-term memories',
-        '  Middle-term memories',
-        '  Fresh memories'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: stats.easiness.longTerm === 0 && stats.easiness.middleTerm === 0 && stats.easiness.shortTerm === 0 ?
-          [0, 0, 1] :
-          [stats.easiness.longTerm, stats.easiness.middleTerm, stats.easiness.shortTerm],
-        backgroundColor: [greenLong, greenMiddle, greenShort],
-        hoverOffset: 0,
-        borderColor: 'rgba(255, 255, 255, 1)',
-        borderWidth: 1,
-        hoverBackgroundColor: [greenLong, greenMiddle, greenShort]
-      }],
-      // responsive: true,
-      // maintainAspectRatio: false
-    };
-
-    const toPercent = (easinessFor) =>
-      stats.nOfProblemsLearned === 0 ?
-        0 :
-        Math.round((stats.easiness[easinessFor] / stats.nOfProblemsLearned) * 100, 2);
-
-    return <div className="stats">
-      <h1>Progress</h1>
-
-      <div className="wrapper">
-        <div className="left" style={{ width: 200, marginTop: 8 }}>
-          <Pie data={data}/>
-        </div>
-
-        <div className="right" style={{ marginTop: 12 }}>
-          <ul className="textual-stats">
-            <li className="stat">
-              {stats.nOfCoursesCreated} courses created
-            </li>
-
-            <li className="stat">
-              {stats.nOfProblemsLearned} flashcards learned:
-              <ul className="memory-levels">
-                <li>
-                  <div className="square" style={{ background: greenLong }}/>
-                  {toPercent('longTerm')}% in long-term memory
-                </li>
-                <li>
-                  <div className="square" style={{ background: greenMiddle }}/>
-                  {toPercent('middleTerm')}% in middle-term memory
-                </li>
-                <li>
-                  <div className="square" style={{ background: greenShort }}/>
-                  {toPercent('shortTerm')}% freshly learned
-                </li>
-              </ul>
-            </li>
-
-            {/* <li className="stat"> */}
-            {/*   Soon: */}
-            {/*   <ul className="memory-levels"> */}
-            {/*     <li>+ 0 flashcards to review in the next hour</li> */}
-            {/*     <li>+ 20 flashcards to review in the next 24 hours</li> */}
-            {/*     <li>+ 300 flashacards to review in the next week</li> */}
-            {/*   </ul> */}
-            {/* </li> */}
-          </ul>
-        </div>
-      </div>
-
-
-    </div>;
-  }
-
   render = () =>
     <Main className={css.main}>
       <div className="space"/>
 
-      <Loading spe={this.state.speGetPage}>{({ user, coursesCreated, skills, stats }) =>
+      <Loading spe={this.state.speGetPage}>{({ user }) =>
         <div className="container">
-          <div className="wrapper">
-            {this.renderUser(user)}
-            {this.renderSkills(skills)}
-            {this.renderStats(stats)}
-          </div>
+          <UserInfo speGetPage={this.state.speGetPage}/>
 
           {
             this.props.currentUser &&
