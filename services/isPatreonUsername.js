058fa925c408195a26442cddba02317890de5f1b
lakesare <lakesare@gmail.com>
2023-04-16 17:51:14 +0400

    patreon - add a new patron user

--- services/isPatreonUsername.js
+++ services/isPatreonUsername.js
@@ -1,4 +1,4 @@
 const isPatreonUsername = (username) =>
-  ['lakesare', 'charlie42', 'daniel-eder', 'inoryy', 'Dhruv B', 'Anders Hagen Jarmund', 'luclu'].includes(username)
+  ['lakesare', 'charlie42', 'daniel-eder', 'inoryy', 'Dhruv B', 'Anders Hagen Jarmund', 'luclu', 'earstick'].includes(username)
 
 export default isPatreonUsername;
