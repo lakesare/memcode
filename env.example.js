5a13f3a9340afeb645224cecf77d39a0939c3d27
lakesare <lakesare@gmail.com>
2023-12-02 19:07:05 +0400

    env.example.js - improve the env file instructions

--- env.example.js
+++ env.example.js
@@ -1,9 +1,19 @@
-// When you import 'env.js', this code gets executed, and variables get added to your environment.
-// (http://thewebivore.com/super-simple-environment-variables-node-js).
-
-// Github Signups
+// Github Signups (NECESSARY)
+//
+// To create these values, go to https://github.com/settings/developers and
+// 1. click "New Oauth App"
+// 2. fill in the values with anything you like, but, importantly:
+//    Homepage URL: http://localhost:3000
+//    Authorization callback URL: http://localhost:3000/api/auth/github/callback
+// 3. Copy "Client ID" (this will give you 'GITHUB_OAUTH_ID') and "Generate a new client secret" (this will give you 'GITHUB_OAUTH_SECRET').
 process.env['GITHUB_OAUTH_ID'] = '';     // Required for local development
 process.env['GITHUB_OAUTH_SECRET'] = ''; // Required for local development
+
+// Database (NECESSARY)
+//
+// To create these values, follow the steps for creating your postgres database from the Readme.md.
+process.env['DB_USER'] = 'postgres';     // Required for local development
+process.env['DB_PASSWORD'] = 'postgres'; // Required for local development
 
 // Google Signups
 process.env['GOOGLE_OAUTH_ID'] = '';
@@ -19,8 +29,5 @@ process.env['AWS_BUCKET_NAME'] = '';
 process.env['CONTACT_EMAIL'] = 'contact@memcode.com';
 
 process.env['JWT_SECRET'] = 'can_be_anything';
-
-process.env['DB_USER'] = '';     // Required for local development
-process.env['DB_PASSWORD'] = ''; // Required for local development
 
 process.env['SENDGRID_API_KEY'] = '';
