caea578ed1e96fbc9ed0ab21d8b7dd8eccc93f9c
Evgenia Karunus <lakesare@gmail.com>
2025-08-28 22:27:06 +0500

    Add detailed vhost debugging

--- backend/middlewares/vhost.js
+++ backend/middlewares/vhost.js
@@ -3,11 +3,22 @@ function vhost(hostname, server){
   if (!hostname) throw new Error('vhost hostname required');
   if (!server) throw new Error('vhost server required');
   var regexp = new RegExp('^' + hostname.replace(/[^*\\w]/g, '\\$&').replace(/[*]/g, '(?:.*?)')  + '$', 'i');
+  console.log('VHOST: Created vhost for hostname:', hostname, 'with regex:', regexp.toString());
   if (server.onvhost) server.onvhost(hostname);
   return function vhost(req, res, next){
-    if (!req.headers.host) return next();
+    if (!req.headers.host) {
+      console.log('VHOST: No host header, calling next()');
+      return next();
+    }
     var host = req.headers.host.split(':')[0];
-    if (!regexp.test(host)) return next();
+    console.log('VHOST: Testing host:', host, 'against hostname:', hostname, 'regex:', regexp.toString());
+    var matches = regexp.test(host);
+    console.log('VHOST: Match result:', matches);
+    if (!matches) {
+      console.log('VHOST: No match, calling next()');
+      return next();
+    }
+    console.log('VHOST: Match found! Routing to server');
     if ('function' == typeof server) return server(req, res, next);
     server.emit('request', req, res);
   };
