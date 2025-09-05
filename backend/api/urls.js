a2fa207f8535be81d5166665b00aa10c0a74da28
Evgenia Karunus <lakesare@gmail.com>
2025-09-06 02:55:42 +0500

    backend - fully converted to a new format!

--- backend/api/urls.js
+++ backend/api/urls.js
@@ -1,52 +1,46 @@
 import router from '#~/router.js';
-
 import catchAsync from '#~/services/catchAsync.js';
 
+import UserApi                  from '#~/api/UserApi/index.js';
+import AuthApi                  from '#~/api/AuthApi/index.js';
+import PageApi                  from '#~/api/PageApi/index.js';
+import AdminApi                 from '#~/api/AdminApi/index.js';
+import CourseApi                from '#~/api/CourseApi/index.js';
+import ProblemApi               from '#~/api/ProblemApi/index.js';
 import NotificationApi          from '#~/api/NotificationApi/index.js';
 import CourseCategoryApi        from '#~/api/CourseCategoryApi/index.js';
 import CourseUserIsLearningApi  from '#~/api/CourseUserIsLearningApi/index.js';
 import ProblemUserIsLearningApi from '#~/api/ProblemUserIsLearningApi/index.js';
-import UserApi                  from '#~/api/UserApi/index.js';
-import ProblemApi               from '#~/api/ProblemApi/index.js';
-import AdminApi                 from '#~/api/AdminApi/index.js';
-import AuthApi                  from '#~/api/AuthApi/index.js';
-import PageApi                  from '#~/api/PageApi/index.js';
-import CourseApi                from '#~/api/CourseApi/index.js';
 
 const getApiClass = (controllerName) => {
   switch (controllerName) {
-    case 'NotificationApi':   return NotificationApi;
-    case 'CourseCategoryApi': return CourseCategoryApi;
-    case 'CourseApi':         return CourseApi;
-    case 'ProblemApi':        return ProblemApi;
-    case 'CourseUserIsLearningApi': return CourseUserIsLearningApi;
+    case 'UserApi':                  return UserApi;
+    case 'AuthApi':                  return AuthApi;
+    case 'PageApi':                  return PageApi;
+    case 'AdminApi':                 return AdminApi;
+    case 'CourseApi':                return CourseApi;
+    case 'ProblemApi':               return ProblemApi;
+    case 'NotificationApi':          return NotificationApi;
+    case 'CourseCategoryApi':        return CourseCategoryApi;
+    case 'CourseUserIsLearningApi':  return CourseUserIsLearningApi;
     case 'ProblemUserIsLearningApi': return ProblemUserIsLearningApi;
-    case 'UserApi':           return UserApi;
-    case 'AdminApi':          return AdminApi;
-    case 'AuthApi':           return AuthApi;
-    case 'PageApi':           return PageApi;
   }
 };
 
-// request.params - { controllerName: 'CourseApi', methodName: 'getPublicCourses' }
 router.post('/api/:controllerName.:methodName', catchAsync(async (request, response, next) => {
   const SomeApi = getApiClass(request.params.controllerName);
   await SomeApi[request.params.methodName](request, response, next);
 }));
 
 router.get('/api/:controllerName.:methodName', catchAsync(async (request, response, next) => {
-  // i want a uniform interface!
   request.body = request.query;
   const SomeApi = getApiClass(request.params.controllerName);
   await SomeApi[request.params.methodName](request, response, next);
 }));
 
-// TODO make all routes like NotificationApi.rate() instead.
-// Everything that's below this line should be deleted eventually.
-
-// AuthApi routes now consolidated - OAuth callbacks still use legacy Express routes
+// MUST STAY AS STANDARD URLS
+// BECAUSE: oauth callbacks
 router.use('/api/auth', AuthApi);
-
-
+// BECAUSE: multipart file uploads with multer middleware
 import FileApi from '#~/api/FileApi/index.js';
 router.use('/api/files', FileApi);
