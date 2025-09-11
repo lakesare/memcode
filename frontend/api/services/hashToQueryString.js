078d96d4e43568b65a43d13e50007c1730eb906d
Evgenia Karunus <lakesare@gmail.com>
2025-09-11 17:08:34 +0500

    ./service/hashToQueryString.js - fix remaining `api.get.` urls

--- frontend/api/services/hashToQueryString.js
+++ frontend/api/services/hashToQueryString.js
@@ -1,8 +1,12 @@
 // @param hash - { SearchQuery: '', pageSize: '', passengerId: 15 }
 // => SearchQuery=Hello&pageSize=3
-const hashToQueryString = (hash) =>
-  Object.keys(hash).map((key) =>
+const hashToQueryString = (hash) => {
+  if (!hash || typeof hash !== 'object') {
+    return '';
+  }
+  return Object.keys(hash).map((key) =>
     key + '=' + encodeURIComponent(hash[key])
   ).join('&');
+};
 
 export default hashToQueryString;
