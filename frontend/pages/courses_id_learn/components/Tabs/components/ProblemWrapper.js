5802d3646f0475df2fc3cbdcaa89b3535bbfb82d
Evgenia Karunus <lakesare@gmail.com>
2025-09-15 22:33:02 +0500

    /courses/:id/learn - revamp /learn page design

--- frontend/pages/courses_id_learn/components/Tabs/components/ProblemWrapper.js
+++ frontend/pages/courses_id_learn/components/Tabs/components/ProblemWrapper.js
@@ -3,6 +3,7 @@ import api from '~/api';
 import MyDuck from '~/ducks/MyDuck';
 
 import Problem from '~/components/Problem';
+import StandardTooltip from '~/components/StandardTooltip';
 
 @connect(
   () => ({}),
@@ -37,11 +38,31 @@ class ProblemWrapper extends React.Component {
   apiIgnore = () => {
     this.props.MyActions.ignoreProblem(this.props.problem.courseId, this.props.problem.id);
 
-    api.post.ProblemUserIsLearningApi.ignoreProblem(
-      (spe) => this.setState({ speIgnore: spe }),
-      { problemId: this.props.problem.id }
-    )
-      .then((puil) => { this.setState({ puil }); });
+    const puil = this.state.puil;
+    
+    if (puil && puil.id) {
+      // Problem is already learned, use ignoreAlreadyLearnedProblem API
+      api.post.ProblemUserIsLearningApi.ignoreAlreadyLearnedProblem(
+        (spe) => this.setState({ speIgnore: spe }),
+        { 
+          problemId: this.props.problem.id,
+          cuilId: puil.courseUserIsLearningId 
+        }
+      )
+        .then(() => { 
+          // Update the local puil state to reflect the ignore
+          this.setState({ 
+            puil: { ...puil, ifIgnored: true } 
+          }); 
+        });
+    } else {
+      // Problem is not learned yet, use regular ignoreProblem API
+      api.post.ProblemUserIsLearningApi.ignoreProblem(
+        (spe) => this.setState({ speIgnore: spe }),
+        { problemId: this.props.problem.id }
+      )
+        .then((puil) => { this.setState({ puil }); });
+    }
   }
 
   apiUnlearnUnignore = () => {
@@ -61,63 +82,114 @@ class ProblemWrapper extends React.Component {
       problemType={this.props.problem.type}
     />
 
-  renderButton = (className, onClick, text, { disabled = false } = {}) =>
-    <div
-      className={`button-wrapper ${className} ${disabled ? '-disabled' : ''}`}
-      onClick={disabled ? () => {} : onClick}
-    >
-      <button className={`button ${className}`} type="button">
-        {
-          (className === '-unlearn' || className === '-unignore') &&
-          <i className="fa fa-undo"/>
-        }
-        {text}
-      </button>
-    </div>
+  renderOptionsDropdown = () => {
+    const puil = this.state.puil;
+    const isDisabled = this.state.speIgnore.status === 'request' || this.state.speDelete.status === 'request';
+    
+    return (
+      <div className="options-dropdown">
+        {puil === false && (
+          <div 
+            className={`option ${isDisabled ? '-disabled' : ''}`}
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent event bubbling
+              if (!isDisabled) this.apiIgnore();
+            }}
+          >
+            <i className="fa fa-eye-slash"/> Ignore
+          </div>
+        )}
+        {puil && puil.ifIgnored === true && (
+          <div 
+            className={`option ${isDisabled ? '-disabled' : ''}`}
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent event bubbling
+              if (!isDisabled) this.apiUnlearnUnignore();
+            }}
+          >
+            <i className="fa fa-undo"/> Unignore
+          </div>
+        )}
+        {puil && puil.ifIgnored !== true && (
+          <>
+            <div 
+              className={`option ${isDisabled ? '-disabled' : ''}`}
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent event bubbling
+                if (!isDisabled) this.apiIgnore();
+              }}
+            >
+              <i className="fa fa-eye-slash"/> Ignore
+            </div>
+            <div 
+              className={`option ${isDisabled ? '-disabled' : ''}`}
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent event bubbling
+                if (!isDisabled) this.apiUnlearnUnignore();
+              }}
+            >
+              <i className="fa fa-undo"/> Unlearn
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+
+  renderThreeDotsMenu = () => {
+    const isDisabled = this.state.speIgnore.status === 'request' || this.state.speDelete.status === 'request';
+    
+    return (
+      <StandardTooltip
+        tooltipEl={this.renderOptionsDropdown()}
+        tooltipProps={{
+          className: 'problem-options-dropdown standard-tooltip -no-padding -dark',
+          interactive: true,
+          placement: 'bottom-end',
+          trigger: 'mouseenter',
+          arrow: false
+        }}
+        width={120}
+      >
+        <button 
+          type="button" 
+          className={`three-dots-menu ${isDisabled ? '-disabled' : ''}`}
+          onClick={(e) => {
+            e.stopPropagation(); // Prevent triggering learn action
+          }}
+        >
+          <i className="fa fa-ellipsis-v"/>
+        </button>
+      </StandardTooltip>
+    );
+  }
 
   render = () => {
-    // pretend that request is already received right after we send it
-    if (this.state.speLearn.status === 'request') {
-      return <div className="problem-wrapper -learned">
-        {this.renderButton('-unlearn', null, 'UNLEARN', { disabled: true })}
-        {this.renderButton('-ignore', null, 'IGNORE', { disabled: true })}
-        {this.renderProblem()}
-      </div>;
-    } else if (this.state.speIgnore.status === 'request') {
-      return <div className="problem-wrapper -ignored">
-        {this.renderButton('-learn', null, 'LEARN', { disabled: true })}
-        {this.renderButton('-unignore', null, 'UNIGNORE', { disabled: true })}
-        {this.renderProblem()}
-      </div>;
-    } else if (this.state.speDelete.status === 'request') {
-      return <div className="problem-wrapper -yet-to-learn">
-        {this.renderButton('-learn', null, 'LEARN', { disabled: true })}
-        {this.renderButton('-ignore', null, 'IGNORE', { disabled: true })}
-        {this.renderProblem()}
-      </div>;
-    }
-
-    // render an actual response
     const puil = this.state.puil;
-    if (puil === false) {
-      return <div className="problem-wrapper -yet-to-learn">
-        {this.renderButton('-learn', this.apiLearn, 'LEARN')}
-        {this.renderButton('-ignore', this.apiIgnore, 'IGNORE')}
-        {this.renderProblem()}
-      </div>;
-    } else if (puil && puil.ifIgnored === true) {
-      return <div className="problem-wrapper -ignored">
-        {this.renderButton('-learn', this.apiLearn, 'LEARN', { disabled: true })}
-        {this.renderButton('-unignore', this.apiUnlearnUnignore, 'UNIGNORE')}
-        {this.renderProblem()}
-      </div>;
-    } else if (puil) {
-      return <div className="problem-wrapper -learned">
-        {this.renderButton('-unlearn', this.apiUnlearnUnignore, 'UNLEARN')}
-        {this.renderButton('-ignore', this.apiIgnore, 'IGNORE', { disabled: true })}
-        {this.renderProblem()}
-      </div>;
+    let wrapperClass = 'problem-wrapper';
+    let onClickHandler = undefined;
+    
+    // Determine wrapper class and click handler based on state
+    if (this.state.speLearn.status === 'request' || (puil && puil.ifIgnored !== true)) {
+      wrapperClass += ' -learned';
+      // Make learned flashcards clickable to unlearn them
+      if (puil && puil.ifIgnored !== true) {
+        onClickHandler = this.apiUnlearnUnignore;
+      }
+    } else if (this.state.speIgnore.status === 'request' || (puil && puil.ifIgnored === true)) {
+      wrapperClass += ' -ignored';
+    } else {
+      wrapperClass += ' -yet-to-learn';
+      // Unlearned flashcards are clickable to learn them
+      onClickHandler = this.apiLearn;
     }
+    
+    return (
+      <div className={wrapperClass} onClick={onClickHandler}>
+        {this.renderThreeDotsMenu()}
+        {this.renderProblem()}
+      </div>
+    );
   }
 }
 
